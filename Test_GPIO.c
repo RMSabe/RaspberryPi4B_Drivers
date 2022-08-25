2ab58ed7e963f03a9548534bb853f0c4136cb382
RMSabe <69878824+RMSabe@users.noreply.github.com>
2022-08-25 10:37:27 -0300

    Update Test_GPIO.c

--- Test_GPIO.c
+++ Test_GPIO.c
@@ -1,3 +1,5 @@
+//A Test executable to test GPIO input/output.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -21,7 +23,7 @@ void delay_us(uint32_t time_us)
 	return;
 }
 
-void led_set(uint8_t value)
+void led_set_output(uint8_t value)
 {
 	value &= 0x0F;
 
@@ -40,13 +42,13 @@ void loop(void)
 	if(!gpio_get_level(BUTTON_0))
 	{
 		led_val--;
-		led_set(led_val);
+		led_set_output(led_val);
 		while(!gpio_get_level(BUTTON_0)) delay_us(1000);
 	}
 	if(!gpio_get_level(BUTTON_1))
 	{
 		led_val++;
-		led_set(led_val);
+		led_set_output(led_val);
 		while(!gpio_get_level(BUTTON_1)) delay_us(1000);
 	}
 
@@ -56,13 +58,26 @@ void loop(void)
 
 int main(int argc, char **argv)
 {
-	if(!gpio_init()) return 0;
+	if(!gpio_init())
+	{
+		printf("GPIO Init Error\nTerminated\n");
+		return 0;
+	}
+	printf("Running...\n");
+	
+	gpio_reset_pin(LED_0);
+	gpio_reset_pin(LED_1);
+	gpio_reset_pin(LED_2);
+	gpio_reset_pin(LED_3);
 
 	gpio_set_pinmode(LED_0, GPIO_PINMODE_OUTPUT);
 	gpio_set_pinmode(LED_1, GPIO_PINMODE_OUTPUT);
 	gpio_set_pinmode(LED_2, GPIO_PINMODE_OUTPUT);
 	gpio_set_pinmode(LED_3, GPIO_PINMODE_OUTPUT);
 
+	gpio_reset_pin(BUTTON_0);
+	gpio_reset_pin(BUTTON_1);
+	
 	gpio_set_pinmode(BUTTON_0, GPIO_PINMODE_INPUT);
 	gpio_set_pinmode(BUTTON_1, GPIO_PINMODE_INPUT);
 	gpio_set_pudctrl(BUTTON_0, GPIO_PUDCTRL_PULLUP);
